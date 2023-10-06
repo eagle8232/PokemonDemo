dfc4d7219e81b411ff08756c51d8c9c04552e496
eagle8232 <zevs.1418@gmail.com>
2023-10-07 02:06:47 +0400

    All done

---
+++ PokemonDemoApp/Model/PokemonModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonModel.swift
+//  PokemonDemoApp
+//
+//  Created by Vusal Nuriyev on 07.10.23.
+//
+
+import Foundation
---
+++ PokemonDemoApp/Model/Pokemons.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemons.swift
+//  PokemonDemoApp
+//
+//  Created by Vusal Nuriyev on 07.10.23.
+//
+
+import Foundation
